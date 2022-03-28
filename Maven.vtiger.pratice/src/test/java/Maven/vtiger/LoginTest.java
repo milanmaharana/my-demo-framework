5ebdb86e836ae2e69958cea707ff5b280a1f36c5
Admin <Admin@DESKTOP-P3KF5V5>
2022-03-28 17:35:53 +0530

    commit

--- Maven.vtiger.pratice/src/test/java/Maven/vtiger/LoginTest.java
+++ Maven.vtiger.pratice/src/test/java/Maven/vtiger/LoginTest.java
@@ -20,36 +20,38 @@ public class LoginTest {
 		String browser= System.getProperty("browser");
 		String username= System.getProperty("username");
 		String password= System.getProperty("password");
+		System.out.println(url);
+		System.out.println(password);
 		//validate 
-		WebDriver driver= null;
-		if(browser.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			 driver=new ChromeDriver();
-		}
-		else if(browser.equalsIgnoreCase("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			 driver=new FirefoxDriver();
-		}
-		// log in browser 
-		driver.get(url);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//input[@name='user_name']")).sendKeys(username);
-		driver.findElement(By.xpath("//input[@name='user_password']")).sendKeys(password);
-		driver.findElement(By.id("submitButton")).click();
-		
-		//log out
-		 WebElement wb =driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']"));
- 		Actions act = new Actions(driver);
- 		act.moveToElement(wb).perform();
- 		driver.findElement(By.linkText("Sign Out")).click();
-		driver.close();
-			 
-		
-		
-		
-		
-		
+//		WebDriver driver= null;
+//		if(browser.equalsIgnoreCase("chrome")) {
+//			WebDriverManager.chromedriver().setup();
+//			 driver=new ChromeDriver();
+//		}
+//		else if(browser.equalsIgnoreCase("firefox")) {
+//			WebDriverManager.firefoxdriver().setup();
+//			 driver=new FirefoxDriver();
+//		}
+//		// log in browser 
+//		driver.get(url);
+//		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//		driver.findElement(By.xpath("//input[@name='user_name']")).sendKeys(username);
+//		driver.findElement(By.xpath("//input[@name='user_password']")).sendKeys(password);
+//		driver.findElement(By.id("submitButton")).click();
+//		
+//		//log out
+//		 WebElement wb =driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']"));
+// 		Actions act = new Actions(driver);
+// 		act.moveToElement(wb).perform();
+// 		driver.findElement(By.linkText("Sign Out")).click();
+//		driver.close();
+//			 
+//		
+//		
+//		
+//		
+//		
 	}	
 
 }
